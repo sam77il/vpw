46baeb87a4e068d95f9be8d96ca83e811af4d5f0
Yavuz Samil Güngör <yavuzsamil.guengoer@gmail.com>
2026-05-15 04:51:40 +0200

    s

--- frontend/src/routes/(root)/login/+page.server.js
+++ frontend/src/routes/(root)/login/+page.server.js
@@ -9,6 +9,9 @@ export const actions = {
 
 		const res = await fetch(`${API_URL}/api/v1/auth/login`, {
 			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
 			body: JSON.stringify({
 				email,
 				password
